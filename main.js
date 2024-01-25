23bf71a58b6c55d172a2729684ec632dd5e32f66
GUBONWOO <zcwxzsx@gmail.com>
2024-01-25 17:05:11 +0900

    part of func2

--- main.js
+++ main.js
@@ -72,3 +72,68 @@ const f = (x) => {
   console.log(x * x);
   return x * x;
 };
+
+//iife
+const l = 7;
+
+const double = () => {
+  console.log(a * 2);
+};
+double();
+
+//Callback
+
+const h = (callback) => {
+  callback();
+  console.log('H');
+};
+const k = () => {
+  console.log('K');
+};
+
+a(b);
+
+const sum2 = (a, b, c) => {
+  setTimeout(() => {
+    c(a + b);
+  }, 1000);
+};
+
+console.log(sum2(1, 2));
+sum2(1, 2, (value) => {
+  console.log(value);
+});
+
+//Recursive
+let i = 0;
+const p = () => {
+  console.log('P');
+  i += 1;
+  if (i < 4) {
+    p();
+  }
+};
+p();
+
+const userM = { name: 'M', parent: null };
+const userO = { name: 'O', parent: userM };
+const userP = { name: 'P', parent: userO };
+
+const getRootUser = (user) => {
+  if (user.parent) {
+    return getRootUser(user.parent);
+  }
+  return user;
+};
+console.log(getRootUser(userP));
+
+//this
+const userX = {
+  firstName: 'Koo',
+  lastName: 'goo',
+  age: 44,
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+console.log(user.getFullName());
